3b8dc32295a7bb8f839c502118609ac0a66755e5
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-10-18 11:35:34 +0200

    dialog popup working

--- client/src/app/add-server-dialog/add-server-dialog.component.ts
+++ client/src/app/add-server-dialog/add-server-dialog.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface ServerInfo {
+  serverName: string;
+  ipAddress: string;
+  environment: string;
+}
 
 @Component({
   selector: 'app-add-server-dialog',
   templateUrl: './add-server-dialog.component.html',
-  styleUrls: ['./add-server-dialog.component.css']
+  styleUrls: ['./add-server-dialog.component.css'],
 })
 export class AddServerDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<AddServerDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ServerInfo
+  ) {}
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSubmit(): void {
+    // Assuming you have a service to handle this data
+    console.log(this.data);
+  }
 }
--- client/src/app/menubar/menubar.component.ts
+++ client/src/app/menubar/menubar.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import {
+  AddServerDialogComponent,
+  ServerInfo,
+} from '../add-server-dialog/add-server-dialog.component';
 
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
-  styleUrls: ['./menubar.component.css']
+  styleUrls: ['./menubar.component.css'],
 })
 export class MenubarComponent {
+  constructor(private dialog: MatDialog) {}
 
+  openDialog(): void {
+    this.dialog.open(AddServerDialogComponent, {
+      width: '260px',
+      data: {},
+    });
+  }
 }
